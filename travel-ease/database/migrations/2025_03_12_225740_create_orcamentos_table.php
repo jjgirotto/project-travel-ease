fe49fb6b55acf2ca143ca4783e59fb1dce543f77
Kamilyszg <kamily.gracia@gmail.com>
2025-03-17 20:25:22 -0300

    feat: Migrations

--- travel-ease/database/migrations/2025_03_12_225740_create_orcamentos_table.php
+++ travel-ease/database/migrations/2025_03_12_225740_create_orcamentos_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('orcamentos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('origem', 100);
+            $table->string('destino', 100);
+            $table->integer('estadia');
+            $table->integer('viajantes');
+            $table->boolean('acomodacao')->default(false);
+            $table->string('preferencia');
+            $table->integer('qtdeMilhas');
+            $table->decimal('valorMilhas', 10,2);
+            $table->decimal('valorTotal', 10,2);
+            $table->boolean('escolhido')->default(false);
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('restrict');
         });
     }
 
