7bbbb92d2754bec7233538660124a22041b48cfe
Kamilyszg <kamily.gracia@gmail.com>
2025-03-17 21:15:51 -0300

    feat: Migrations

--- travel-ease/database/migrations/2025_03_12_231656_create_passagems_table.php
+++ travel-ease/database/migrations/2025_03_12_231656_create_passagems_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('passagems', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->date('checkin');
+            $table->date('checkout');
+            $table->string('aeroIrigem', 100);
+            $table->string('aeroDestino', 100);
+            $table->foreign('viagem_id')->references('id')->on('viagems')->onDelete('restrict');
         });
     }
 
