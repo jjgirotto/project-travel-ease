a4e9f6565d3cc1eff0d84f787f9fcd09ae032697
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-04-05 15:16:35 -0300

    refactor: passagens e orcamento views

--- travel-ease/resources/views/orcamentos/show.blade.php
+++ travel-ease/resources/views/orcamentos/show.blade.php
@@ -36,32 +36,32 @@
 
     <div class="mb-3">
         <label for="acomodacao" class="form-label">Precisa de acomodação?</label>
-        <input type="checkbox" id="acomodacao" name="acomodacao" class="form-check-input" value="1" {{ $orcamento->acomodacao ? 'checked' : '' }}>
+        <input type="checkbox" id="acomodacao" name="acomodacao" class="form-check-input" disabled value="1" {{ $orcamento->acomodacao ? 'checked' : '' }}>
     </div>
         
         <div class="mb-3">
             <label for="preferencia" class="form-label">Preferência:</label>
-            <input type="text" id="preferencia" name="preferencia" value = "{{ $orcamento->preferencia }}" class="form-control">
+            <input type="text" id="preferencia" name="preferencia" value = "{{ $orcamento->preferencia }}" class="form-control" disabled>
         </div>
 
         <div class="mb-3">
             <label for="qtdeMilhas" class="form-label">Quantidade de milhas:</label>
-            <input type="number" id="qtdeMilhas" name="qtdeMilhas" value = "{{ $orcamento->qtdeMilhas }}" class="form-control">
+            <input type="number" id="qtdeMilhas" name="qtdeMilhas" value = "{{ $orcamento->qtdeMilhas }}" class="form-control" disabled>
         </div>
 
         <div class="mb-3">
             <label for="valorMilhas" class="form-label">Valor de milhas:</label>
-            <input type="number" id="valorMilhas" name="valorMilhas" step="0.01" value = "{{ $orcamento->valorMilhas }}" class="form-control">
+            <input type="number" id="valorMilhas" name="valorMilhas" step="0.01" value = "{{ $orcamento->valorMilhas }}" class="form-control" disabled>
         </div>
 
         <div class="mb-3">
             <label for="valorTotal" class="form-label">Valor total:</label>
-            <input type="number" id="valorTotal" name="valorTotal" step="0.01" value = "{{ $orcamento->valorTotal }}" class="form-control">
+            <input type="number" id="valorTotal" name="valorTotal" step="0.01" value = "{{ $orcamento->valorTotal }}" class="form-control" disabled>
         </div>
 
     <div class="mb-3">
         <label for="escolhido" class="form-label">Orçamento escolhido:</label>
-        <input type="checkbox" id="escolhido" name="escolhido" class="form-check-input" value="1" {{ $orcamento->escolhido ? 'checked' : '' }}>
+        <input type="checkbox" id="escolhido" name="escolhido" class="form-check-input" value="1" {{ $orcamento->escolhido ? 'checked' : '' }} disabled>
     </div>
 
         <div class="mb-3">
--- travel-ease/resources/views/pacoteViagens/show.blade.php
+++ travel-ease/resources/views/pacoteViagens/show.blade.php
@@ -15,17 +15,17 @@
 
       <div class="mt-3">
           <label for="passeios" class="form-label">Passeios:</label>
-          <input type="text" name="passeios" id="passeios" value = "{{ $pacote->passeios }}" class="form-control">
+          <input type="text" name="passeios" id="passeios" value = "{{ $pacote->passeios }}" class="form-control" disabled>
       </div>
 
       <div class="mt-3">
           <label for="restaurantes" class="form-label">Restaurantes</label>
-          <input type="text" name="restaurantes" id="restaurantes" value = "{{ $pacote->restaurantes }}" class="form-control">
+          <input type="text" name="restaurantes" id="restaurantes" value = "{{ $pacote->restaurantes }}" class="form-control" disabled>
       </div>
 
       <div class="mt-3 mb-3">
             <label for="viagem_id" class="form-label">Viagem: </label>
-            <select id="viagem_id" name="viagem_id" class="form-select" required="">
+            <select id="viagem_id" name="viagem_id" class="form-select" disabled>
                 @foreach ($viagens as $v)
                     <option value="{{ $v->id }}" {{ $pacote->viagem_id == $v->id ? "selected" : ""}} >
                         {{ $v->orcamento->origem }} x {{ $v->orcamento->destino }}: {{ $v->orcamento->cliente->nome }} - CPF: {{ $v->orcamento->cliente->cpf }}
